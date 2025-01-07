albjeremias/deltachat-android

commit 14f69f87e8b11a7d0568e58593946a1f1c01e75e

fix: Use the correct file name in MediaView

src/main/java/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -253,8 +253,8 @@ public class AttachmentManager {
       @Override
       protected @Nullable Slide doInBackground(Void... params) {
         try {
-          if (msg != null && msg.getType() == DcMsg.DC_MSG_WEBXDC) {
-            return new DocumentSlide(context, msg);
+          if (msg != null) {
+            return MediaUtil.getSlideForMsg(context, msg);
           }
           else if (PartAuthority.isLocalUri(uri)) {
             return getManuallyCalculatedSlideInfo(uri, width, height, msg);
@@ -374,7 +374,7 @@ public class AttachmentManager {
 
         if (PartAuthority.isLocalUri(uri)) {
           mediaSize = PartAuthority.getAttachmentSize(context, uri);
-          if (fileName != null) fileName  = PartAuthority.getAttachmentFileName(context, uri);
+          if (fileName != null) fileName = PartAuthority.getAttachmentFileName(context, uri);
           mimeType  = PartAuthority.getAttachmentContentType(context, uri);
         }
 
@@ -680,10 +680,10 @@ public class AttachmentManager {
       }
 
       switch (this) {
-      case IMAGE:    return new ImageSlide(context, uri, dataSize, width, height);
-      case GIF:      return new GifSlide(context, uri, dataSize, width, height);
+      case IMAGE:    return new ImageSlide(context, uri, fileName, dataSize, width, height);
+      case GIF:      return new GifSlide(context, uri, fileName, dataSize, width, height);
       case AUDIO:    return new AudioSlide(context, uri, dataSize, false, fileName);
-      case VIDEO:    return new VideoSlide(context, uri, dataSize);
+      case VIDEO:    return new VideoSlide(context, uri, fileName, dataSize);
       case DOCUMENT:
         // We have to special-case Webxdc slides: The user can interact with them as soon as a draft
         // is set. Therefore we need to create a DcMsg already now.

src/main/java/org/thoughtcrime/securesms/MediaPreviewActivity.java
@@ -520,7 +520,7 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity
       MediaView mediaView = itemView.findViewById(R.id.media_view);
 
       try {
-        mediaView.set(glideRequests, window, uri, mediaType, size, true);
+        mediaView.set(glideRequests, window, uri, name, mediaType, size, true);
       } catch (IOException e) {
         Log.w(TAG, e);
       }
@@ -605,7 +605,7 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity
 
       try {
         //noinspection ConstantConditions
-        mediaView.set(glideRequests, window, Uri.fromFile(msg.getFileAsFile()),
+        mediaView.set(glideRequests, window, Uri.fromFile(msg.getFileAsFile()), msg.getFilename(),
             msg.getFilemime(), msg.getFilebytes(), autoplay);
       } catch (IOException e) {
         Log.w(TAG, e);

src/main/java/org/thoughtcrime/securesms/components/MediaView.java
@@ -3,10 +3,10 @@ package org.thoughtcrime.securesms.components;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
+
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.Window;
@@ -55,6 +55,7 @@ public class MediaView extends FrameLayout {
   public void set(@NonNull GlideRequests glideRequests,
                   @NonNull Window window,
                   @NonNull Uri source,
+                  @Nullable String fileName,
                   @NonNull String mediaType,
                   long size,
                   boolean autoplay)
@@ -68,7 +69,7 @@ public class MediaView extends FrameLayout {
       imageView.setVisibility(View.GONE);
       videoView.get().setVisibility(View.VISIBLE);
       videoView.get().setWindow(window);
-      videoView.get().setVideoSource(new VideoSlide(getContext(), source, size), autoplay);
+      videoView.get().setVideoSource(new VideoSlide(getContext(), source, fileName, size), autoplay);
     } else {
       throw new IOException("Unsupported media type: " + mediaType);
     }

src/main/java/org/thoughtcrime/securesms/mms/GifSlide.java
@@ -14,8 +14,8 @@ public class GifSlide extends ImageSlide {
     super(context, dcMsg);
   }
 
-  public GifSlide(Context context, Uri uri, long size, int width, int height) {
-    super(context, constructAttachmentFromUri(context, uri, MediaUtil.IMAGE_GIF, size, width, height, uri, null, false));
+  public GifSlide(Context context, Uri uri, String fileName, long size, int width, int height) {
+    super(context, constructAttachmentFromUri(context, uri, MediaUtil.IMAGE_GIF, size, width, height, uri, fileName, false));
   }
 
   @Override

src/main/java/org/thoughtcrime/securesms/mms/ImageSlide.java
@@ -39,8 +39,8 @@ public class ImageSlide extends Slide {
     super(context, attachment);
   }
 
-  public ImageSlide(Context context, Uri uri, long size, int width, int height) {
-    super(context, constructAttachmentFromUri(context, uri, MediaUtil.IMAGE_JPEG, size, width, height, uri, null, false));
+  public ImageSlide(Context context, Uri uri, String fileName, long size, int width, int height) {
+    super(context, constructAttachmentFromUri(context, uri, MediaUtil.IMAGE_JPEG, size, width, height, uri, fileName, false));
   }
 
   @Override

src/main/java/org/thoughtcrime/securesms/mms/VideoSlide.java
@@ -30,16 +30,16 @@ import java.io.File;
 
 public class VideoSlide extends Slide {
 
-  private static Attachment constructVideoAttachment(Context context, Uri uri, long dataSize)
+  private static Attachment constructVideoAttachment(Context context, Uri uri, String fileName, long dataSize)
   {
     Uri thumbnailUri = Uri.fromFile(new File(DcHelper.getBlobdirFile(DcHelper.getContext(context), "temp-preview.jpg")));
     MediaUtil.ThumbnailSize retWh = new MediaUtil.ThumbnailSize(0, 0);
     MediaUtil.createVideoThumbnailIfNeeded(context, uri, thumbnailUri, retWh);
-    return constructAttachmentFromUri(context, uri, MediaUtil.VIDEO_UNSPECIFIED, dataSize, retWh.width, retWh.height, thumbnailUri, null, false);
+    return constructAttachmentFromUri(context, uri, MediaUtil.VIDEO_UNSPECIFIED, dataSize, retWh.width, retWh.height, thumbnailUri, fileName, false);
   }
 
-  public VideoSlide(Context context, Uri uri, long dataSize) {
-    super(context, constructVideoAttachment(context, uri, dataSize));
+  public VideoSlide(Context context, Uri uri, String fileName, long dataSize) {
+    super(context, constructVideoAttachment(context, uri, fileName, dataSize));
   }
 
   public VideoSlide(Context context, DcMsg dcMsg) {

commit 48f5a6f28215b08b91f41cc72c33f5b83e16ad5b

fix: Fix drafting images
This fixes a bug introduced in 14f69f87e8b11a7d0568e58593946a1f1c01e75e:
When you drafted an image, pressed Back, and opened the chat again, then
the height of the drafted image was wrong and tapping the image opened a
preview for the wrong image.

I do think that theoretically it would be nicer to use getSlideForMsg
here, because we already have a DcMsg, but this didn't work because a)
the width and height wasn't gotten from the msg and instead 0 was passed
and b) the code tries to save a msgId instead of the message instead,
and loading the message from the database fails later since it's just a
draft.

I didn't want to try and fix these things, because they might be bigger
refactorings and I don't know the code.

src/main/java/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -253,8 +253,8 @@ public class AttachmentManager {
       @Override
       protected @Nullable Slide doInBackground(Void... params) {
         try {
-          if (msg != null) {
-            return MediaUtil.getSlideForMsg(context, msg);
+          if (msg != null && msg.getType() == DcMsg.DC_MSG_WEBXDC) {
+            return new DocumentSlide(context, msg);
           }
           else if (PartAuthority.isLocalUri(uri)) {
             return getManuallyCalculatedSlideInfo(uri, width, height, msg);
@@ -374,7 +374,7 @@ public class AttachmentManager {
 
         if (PartAuthority.isLocalUri(uri)) {
           mediaSize = PartAuthority.getAttachmentSize(context, uri);
-          if (fileName != null) fileName = PartAuthority.getAttachmentFileName(context, uri);
+          if (fileName == null) fileName = PartAuthority.getAttachmentFileName(context, uri);
           mimeType  = PartAuthority.getAttachmentContentType(context, uri);
         }
 